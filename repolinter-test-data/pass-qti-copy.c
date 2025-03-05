b837e856e1febc8e315f481285de90cd96f6e4e8
Mark Matyas <mmatyas@qti.qualcomm.com>
2025-03-05 10:56:39 -0800

    Add repolinter config, workflow template, etc
    Signed-off-by: Mark Matyas <mmatyas@qti.qualcomm.com>

---
+++ repolinter-test-data/pass-qti-copy.c
@@ -0,0 +1,8 @@
+/*
+
+Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. 
+All rights reserved.
+Confidential and Proprietary - Qualcomm Technologies, Inc.
+
+SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
